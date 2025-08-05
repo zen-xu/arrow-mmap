2513736bc6c07bb809cda5819c6c5fd8d1115af7
ZhengYu, Xu <zen-xu@outlook.com>
2025-08-05 15:19:26 +0800

    update benchmark

--- benchmark/benchmark_mmap_db.cpp
+++ benchmark/benchmark_mmap_db.cpp
@@ -4,6 +4,7 @@
 #include <quill/Frontend.h>
 #include <quill/sinks/ConsoleSink.h>
 
+#include "mmap/dyn_partition_db.hpp"
 #include "mmap/partition_db.hpp"
 
 struct Data0 {
@@ -15,25 +16,70 @@ struct Data1 {
   double c;
 };
 
-// 基准测试：写入性能
 static void BM_WritePerformance(benchmark::State& state) {
-  quill::Backend::start();
-  auto logger =
-      quill::Frontend::create_or_get_logger("default", quill::Frontend::create_or_get_sink<quill::ConsoleSink>("sink"));
-
   auto db = mmap_db::PartitionDB<std::tuple<Data0, Data1>>("benchmark_db");
   int capacity = 10000000;
   db.truncate_or_create(capacity, true);
   auto writer = db.writer<0>();
 
+  Data0 data0{1, 1, 1, 1};
   for (auto _ : state) {
     for (int i = 0; i < capacity; i++) {
-      writer.write({1, 1, 1, 1}, i);
+      writer.write(data0, i);
+    }
+  }
+}
+
+static void BM_DynWritePerformance(benchmark::State& state) {
+  auto db = mmap_db::DynPartitionDB("benchmark_dyn_db");
+  int capacity = 10;
+  db.create(capacity, {sizeof(Data0)});
+  auto writer = db.writer(0);
+
+  Data0 data0{1, 1, 1, 1};
+  for (auto _ : state) {
+    for (int i = 0; i < capacity; i++) {
+      writer.write(&data0, i);
+    }
+  }
+}
+
+static void BM_DynWritePerformance2(benchmark::State& state) {
+  auto db = mmap_db::DynPartitionDB("benchmark_dyn_db");
+  int capacity = 10;
+  db.create(capacity, {sizeof(Data0)});
+  auto writer = db.writer(0);
+
+  Data0 data0{1, 1, 1, 1};
+  auto addr = writer.addr();
+  for (auto _ : state) {
+    for (int i = 0; i < capacity; i++) {
+      std::memcpy(addr + i * sizeof(Data0), &data0, sizeof(Data0));
     }
   }
 }
 
 // 注册基准测试
-BENCHMARK(BM_WritePerformance)->Iterations(100);
+// BENCHMARK(BM_WritePerformance)->Iterations(100);
+// BENCHMARK(BM_DynWritePerformance)->Iterations(100000000);
+BENCHMARK(BM_DynWritePerformance)->Iterations(100000000);
+BENCHMARK(BM_DynWritePerformance2)->Iterations(100000000);
 
-BENCHMARK_MAIN();
+int main(int argc, char** argv) {
+  benchmark::MaybeReenterWithoutASLR(argc, argv);
+  char arg0_default[] = "benchmark";
+  char* args_default = reinterpret_cast<char*>(arg0_default);
+  if (!argv) {
+    argc = 1;
+    argv = &args_default;
+  }
+  quill::Backend::start();
+  auto logger =
+      quill::Frontend::create_or_get_logger("default", quill::Frontend::create_or_get_sink<quill::ConsoleSink>("sink"));
+
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+  return 0;
+}

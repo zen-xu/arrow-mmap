8bafec214ad036ed6a012f268667eec8c6a17ab3
ZhengYu, Xu <zen-xu@outlook.com>
2025-08-22 17:44:45 +0800

    move ArrowWriter logic into cpp

--- src/mmap_arrow/arrow_writer.hpp
+++ src/mmap_arrow/arrow_writer.hpp
@@ -11,44 +11,7 @@ namespace mmap_arrow {
 
 class ArrowWriter {
  public:
-  ArrowWriter(const size_t id, const ArrowMeta meta, const IMmapWriter* data_writer, const IMmapWriter* bitmap_writer)
-      : id(id),
-        meta_(meta),
-        data_writer_(data_writer),
-        bitmap_writer_(bitmap_writer),
-        write_rows([id, meta]() {
-          if (id < meta.writer_count - 1) {
-            return meta.array_length / meta.writer_count;
-          } else {
-            return meta.array_length - meta.array_length / meta.writer_count * (meta.writer_count - 1);
-          }
-        }()),
-        col_sizes_([&]() {
-          std::vector<size_t> col_sizes;
-          for (const auto& field : meta.schema->fields()) {
-            col_sizes.push_back(field->type()->byte_width());
-          }
-          return col_sizes;
-        }()),
-        col_array_sizes_([&]() {
-          std::vector<size_t> col_array_sizes;
-          // Because except for the last writer, the num_rows of other writers is the same
-          auto num_rows = meta_.array_length / meta_.writer_count;
-          for (const auto& size : col_sizes_) {
-            col_array_sizes.push_back(size * num_rows);
-          }
-          return col_array_sizes;
-        }()),
-        col_array_offsets_([&]() {
-          std::vector<size_t> col_array_offsets;
-          for (size_t i = 0; i < col_sizes_.size(); i++) {
-            col_array_offsets.push_back(std::accumulate(col_sizes_.begin(), col_sizes_.begin() + i, 0) *
-                                        meta_.array_length);
-          }
-          return col_array_offsets;
-        }()),
-        batch_size_(std::accumulate(col_sizes_.begin(), col_sizes_.end(), 0) * meta_.array_length) {}
-
+  ArrowWriter(const size_t id, const ArrowMeta meta, const IMmapWriter* data_writer, const IMmapWriter* bitmap_writer);
   bool write(const std::shared_ptr<arrow::RecordBatch>& batch);
   bool write(const std::shared_ptr<arrow::RecordBatch>& batch, size_t index);
 

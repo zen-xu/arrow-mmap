672d78e85d9763fdcb227c29f1ff1c15821cf2d3
ZhengYu, Xu <zen-xu@outlook.com>
2025-08-26 17:00:12 +0800

    fix pyarrow open file

--- example/pd_load.py
+++ example/pd_load.py
@@ -2,6 +2,6 @@ import pyarrow.ipc as pa_ipc
 
 
 with open("stream.arrow", "rb") as f:
-    df = pa_ipc.open_stream(f).read_all().to_pandas()
+    df = pa_ipc.open_file(f).read_all().to_pandas()
 
 print(df)
